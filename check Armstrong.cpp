7da5ea2e6e0982cd6db37dee45298e7c8aa73dab
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-11 17:49:41 +0530

    Easy

---
+++ check Armstrong.cpp
@@ -0,0 +1,15 @@
+bool checkArmstrong(int n){
+	//Write your code here
+	int duplicate = n;
+	int ans = 0;
+	while(n != 0){
+		int remainder = n % 10;
+		
+		ans = ans + (remainder*remainder*remainder);
+		n /= 10;
+	}
+	if(ans != duplicate){
+		return false;
+	}
+	return true;
+}
