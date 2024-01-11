20994ea15853ed426a5b67e7edd388b9c8c0856a
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-11 17:02:59 +0530

    Easy

---
+++ 9.Palindrome number .cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+       int copy = x ;
+       int ans = 0;
+       
+       while(x != 0) {
+           int reminder = x % 10;
+           if((ans > INT_MAX/10) || (ans == INT_MAX/10 && x>7))
+           return false;
+           ans = ans * 10  + reminder;
+           x /= 10;
+       }
+       if(ans != copy){
+           return false;
+       }
+       else {
+           return true;
+       }
+    }
+};
