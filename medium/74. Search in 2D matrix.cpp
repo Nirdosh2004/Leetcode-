8dc3a6d4443929e7bd62646e1a5f5132351d2b7a
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-24 21:29:28 +0530

    Create 74. Search in 2D matrix.cpp

---
+++ medium/74. Search in 2D matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+      //TC = O(log(n*m));
+      //SC = O(1)
+      
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int start = 0;
+        int end = n* m - 1;
+        while(start <= end){
+            int mid = start + (end - start )/2;
+            int row = mid/m;
+            int col = mid%m;
+            if(matrix[row][col]==target)return true;
+            else if(matrix[row][col] < target)start = mid + 1;
+            else{
+                end = mid  - 1;
+            }
+        }
+        return false;
+    }
+};
