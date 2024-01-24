fed40469c3abde887996f496125dfe11fd8855e9
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-24 21:44:11 +0530

    Create 240. Search a 2D matrix II .cpp

---
+++ medium/240. Search a 2D matrix II .cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+//       Time complexity:O(log(n*m))
+// Space complexity:O(1)
+      
+        int start = 0;
+        int end = matrix[0].size()-1;
+        while(start < matrix.size() && end >= 0){
+            if(matrix[start][end]==target){
+                return true;
+            }
+            else if(matrix[start][end] > target){
+                end--;
+            }
+            else{
+                start++;
+            }
+        }
+        return false;
+    }
+};
