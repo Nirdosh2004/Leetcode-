6b88c6b7e716e1411d25994237238012bbe5187c
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-03-17 12:45:22 +0530

    Update 4. Median of two sorted array.cpp

--- 4. Median of two sorted array.cpp
+++ 4. Median of two sorted array.cpp
@@ -6,7 +6,7 @@ public:
         vector<int>merged ;
         for(int i=0;i<m;i++){
             merged.push_back(nums1[i]);
-        }
+        } 
         for(int i =0;i<n;i++){
             merged.push_back(nums2[i]);
         }
