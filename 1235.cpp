e357faf5ee425f815215951681adf4a554db90ed
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-05-18 21:35:54 +0530

    Update 1235.cpp

--- 1235.cpp
+++ 1235.cpp
@@ -4,7 +4,7 @@ public:
         int numJobs = profit.size(); // Number of jobs
         vector<tuple<int, int, int>> jobs(numJobs);
       
-        for (int i = 0; i < numJobs; ++i) {        
+        for (int i = 0; i < numJobs; ++i) {           
             jobs[i] = {endTime[i], startTime[i], profit[i]};
         }
           
